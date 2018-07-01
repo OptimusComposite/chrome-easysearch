d89bcd3a40800c30492acee418f63d655a3ee38e
Fei <optimusgrass@live.com>
2018-07-01 21:52:54 +0800

    ...

--- source/scripts/search.js
+++ source/scripts/search.js
@@ -1,3 +1,12 @@
-function run(){
-    alert('s')
+
+window.onload = function () {
+    let newtab = document.getElementById('searchbutton')
+
+    newtab.onclick = function () {
+        let searchengine = "https://www.google.com/search?newwindow=1"
+        let searchsite = "stackoverflow.com"
+        let searchkeyword = document.getElementById('keyword').value
+        let myurl = searchengine + "&q=site%3A" + searchsite + "+" + searchkeyword
+        chrome.tabs.create({url: myurl})
+    }
 }
